1e4e1323b4cc3574cae1a4b09cce376d214da987
Henry <henry.alexandre-dos-santos@epitech.eu>
2024-02-21 18:50:55 +0100

    LOGIN PAGE FRONT

--- front/login.php
+++ front/login.php
@@ -5,61 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <style><?php include "./css/normalize.css"?></style>
     <style><?php include "./css/output.css"?></style>
-    <title>Document</title>
+    <title>LOGIN</title>
   </head>
-  <body class="bg-black 2xl:text-lg	text-sm">
-    <main class="flex flex-col justify-between h-screen">
-      <div class="flex items-center ">
-        <!-- <div class="flex grow-0 shrink-0 basis-1/2"> -->
-          <img
-            src="img/X_logo_2023_(white).png"
-            alt="Logo"
-            class="max-w-full flex grow-0 shrink-0 basis-1/2"
-          />
-        <!-- </div> -->
-        <div class="grow-0 shrink-0 basis-3/6	">
-          <!-- <h2>Sign in to</h2> -->
-          <form action="" method="POST" class="flex items-center flex-col">
-            <input
-              type="email"
-              placeholder="EMAIL"
-              name="email"
-              id="email"
-              class="mb-8 py-2 px-6 bg-black rounded-xl text-white border border-solid border-gray-500 w-4/5"
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="PASSWORD"
-              id="password"
-              class="mb-8 py-2 px-6 bg-black rounded-xl text-white border border-solid border-gray-500 w-4/5"
-              required
-            />
-            <input
-              type="submit"
-              name="envoyer"
-              class="button mb-8 py-2 px-6 bg-white text-black rounded-xl border border-solid border-gray-500 opacity-50 w-3/5 cursor-pointer"
-              id="submit"
-              value="CONNEXION"
-              required
-            />
-            <a href="#" class="no-underline text-white mb-8"
-              >FORGOT PASSWORD ?</a
-            >
-            <hr class="mb-8 border border-solid border-gray-500 w-4/5" />
-            <a href="#" class="no-underline text-white mb-8"
-              >NOT REGISTER YET ?</a
-            >
+  <body class="bg-black md:flex">
+      <img class="lg:h-[calc(100vh-3rem)] lg:w-[calc(100%/2)]" src="./img/X_logo_2023_(white).png" alt="Logo">
+      <div class="lg:h-[calc(100vh-3rem)] lg:w-[calc(100%/2)] flex flex-col justify-center items-center">
+          <h1 class="text-4xl font-bold text-white pb-14">LOGIN</h1>
+          <form action="login.php" method="post" class="flex flex-col space-y-4 w-full max-w-md">
+              <input type="text" name="username" placeholder="Username" class="p-2 rounded-md bg-gray-800 text-white">
+              <input type="password" name="password" placeholder="Password" class="p-2 rounded-md bg-gray-800 text-white">
+              <button type="submit" class="p-2 bg-white rounded-md hover:bg-white/80 duration-300">Login</button>
           </form>
-        </div>
       </div>
-      <footer
-        class="flex justify-end items-center border border-solid border-gray-500 h-20"
-      >
-        <p class="mr-20 text-white">© WAC 2024</p>
-      </footer>
-    </main>
-    <script><?php include "../node_modules/flowbite/dist/flowbite.min.js"?></script>
+      <script><?php include "../node_modules/flowbite/dist/flowbite.min.js"?></script>
   </body>
 </html>
