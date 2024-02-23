be194aca2abd67ca7ada3b99347ebf0b4e1dd66f
MassiTgh <massinissa.tighdine@epitech.eu>
2024-02-23 10:54:37 +0100

    modif myaccount

--- front/index.php
+++ front/index.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 
-
+<a href="./myaccount.php">Myaccount</a>
 <nav class="bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
     <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
