2e937093ac50a8252e27b8a847b283548c4b4255
root <root@LAPTOP-PI4MQAGH>
2024-03-10 21:03:09 +0100

    follow_follower_following_done

---
+++ back/get_followings.php
@@ -0,0 +1,14 @@
+<?php 
+    function get_followings(): array | false{
+        session_start(); 
+        $pdo = require("database.php");
+        if (!isset($_SESSION['user_id'])) {
+            header("Location : ../front/login.php");
+        }else{
+            $id_user = $_SESSION['user_id'];
+             $statement_followings = $pdo->query("SELECT * FROM follow JOIN user ON user.id = follow.id_user WHERE id_follow = $id_user;");
+            $resultat_followings = $statement_followings->fetchAll(PDO::FETCH_ASSOC);
+        }
+        return $resultat_followings ?? false;
+    }
+  
