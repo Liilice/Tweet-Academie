c6b552bf0c01791d5258995e913d26ea005bf6bc
Henry <henry.alexandre-dos-santos@epitech.eu>
2024-03-11 11:12:11 +0100

    Change follows and following

--- back/get_followings.php
+++ back/get_followings.php
@@ -6,7 +6,7 @@
             header("Location : ../front/login.php");
         }else{
             $id_user = $_SESSION['user_id'];
-             $statement_followings = $pdo->query("SELECT * FROM follow JOIN user ON user.id = follow.id_user WHERE id_follow = $id_user;");
+             $statement_followings = $pdo->query("SELECT * FROM follow JOIN user ON user.id = follow.id_follow WHERE id_user = $id_user;");
             $resultat_followings = $statement_followings->fetchAll(PDO::FETCH_ASSOC);
         }
         return $resultat_followings ?? false;
