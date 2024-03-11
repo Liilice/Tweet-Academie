ef0069963aa6cef2aa5774f8d221ed2348430e09
MassiTgh <massinissa.tighdine@epitech.eu>
2024-03-11 11:16:59 +0100

    modif

--- back/database.php
+++ back/database.php
@@ -2,15 +2,10 @@
     $dsn = 'mysql:host=localhost;dbname=twitter';
     $user = 'Massi';
     $password = 'qa58WS05massinissa';
-    try
-    {
+    try{
         $pdo = new PDO($dsn, $user, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    }
-    
-    catch(PDOException $e)
-    {
+    }catch(PDOException $e){
         echo "Erreur : ".$e->getMessage();
     }
     return $pdo;
